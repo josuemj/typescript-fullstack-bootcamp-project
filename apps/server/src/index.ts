333e6fa225ce127e7d2c7e7621f53bdbd38774dd
josuemj <98498772+josuemj@users.noreply.github.com>
2024-11-21 23:12:08 -0600

    feat: created default endpoint sample

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,25 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '../../../packages/database/prisma/prisma-client'
 
 const app = express()
+const client = new PrismaClient()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (_, res) => {
-  return res.json({ ok: true })
+app.get('/', async(_, res) => {
+  const result = await client.product.findMany({
+    include : {
+      variants: true,
+    },
+    where: {
+      id: 5
+    }
+  })
+  return res.json({ result })
 })
 
 const port = process.env.PORT || 5001
