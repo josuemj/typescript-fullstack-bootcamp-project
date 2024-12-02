295589812fccdfb03aac466c611abc4f790c2c79
josuemj <98498772+josuemj@users.noreply.github.com>
2024-12-01 20:36:08 -0600

    style: side bar

--- apps/client/src/components/sidebar/SideBar.tsx
+++ apps/client/src/components/sidebar/SideBar.tsx
@@ -1,13 +1,12 @@
-import './SideBar.css'
-import { CollectionsDropDown } from './CollectionsDropDown'
-import { SortDropDown } from './SortDropDown'
+import './SideBar.css';
+import { CollectionsDropDown } from './CollectionsDropDown';
+import { SortDropDown } from './SortDropDown';
 
 type SideBarProps = {
   collections: [] | any;
   onCollectionChange: (collectionId: number | string) => void;
-  onSortChange: (sortOrder: string) => void; // Callback for sort order
+  onSortChange: (sortOrder: string) => void;
 };
-
 
 export const SideBar = ({
   collections,
@@ -17,13 +16,17 @@ export const SideBar = ({
   return (
     <div className="sideBar">
       <h3>Menu</h3>
-      <h3>Show</h3>
-      <CollectionsDropDown
-        CollectionsFetch={collections}
-        onChange={onCollectionChange}
-      />
-      <h3>Sort</h3>
-      <SortDropDown onSortChange={onSortChange} />
+      <div>
+        <h3>Show</h3>
+        <CollectionsDropDown
+          CollectionsFetch={collections}
+          onChange={onCollectionChange}
+        />
+      </div>
+      <div>
+        <h3>Sort</h3>
+        <SortDropDown onSortChange={onSortChange} />
+      </div>
     </div>
   );
 };
